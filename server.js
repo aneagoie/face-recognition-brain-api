b993818d06411aaf40bb19e7f526f0b700e97ed1
juangom73 <juangom73@gmail.com>
2020-01-05 21:42:40 -0500

    Finishing Code

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ const db = knex({
   }
 });
 
-console.log(db.select('*').from('users'));
+//console.log(db.select('*').from('users'));
 
 const app = express();
 
@@ -29,13 +29,14 @@ app.get('/', (req, res)=> {
 })
 
 app.post('/signin', (req, res) => {
+  //console.log("Email: ", req.body.email);
   db.select('email', 'hash').from('login')
-    .where('email', '=', req.body.email)
+    .where('email', '=', req.body.username)
     .then(data => {
       const isValid = bcrypt.compareSync(req.body.password, data[0].hash);
       if (isValid) {
         return db.select('*').from('users')
-          .where('email', '=', req.body.email)
+          .where('email', '=', req.body.username)
           .then(user => {
             res.json(user[0])
           })
@@ -44,12 +45,14 @@ app.post('/signin', (req, res) => {
         res.status(400).json('wrong credentials')
       }
     })
-    .catch(err => res.status(400).json('wrong credentials'))
+    .catch(err => res.status(400).json('wrong credentials test'))
 })
 
 app.post('/register', (req, res) => {
   const { email, name, password } = req.body;
   const hash = bcrypt.hashSync(password);
+
+// Transaction is used to make sure that all the procedure was successful. If something fails, then all the other process will fail
     db.transaction(trx => {
       trx.insert({
         hash: hash,
@@ -58,6 +61,7 @@ app.post('/register', (req, res) => {
       .into('login')
       .returning('email')
       .then(loginEmail => {
+        //We add here trx to be sure that it is part of the transaction
         return trx('users')
           .returning('*')
           .insert({
@@ -69,7 +73,9 @@ app.post('/register', (req, res) => {
             res.json(user[0]);
           })
       })
+      //We need to commit the transaction to make it works
       .then(trx.commit)
+      //If there is an error then it will rollback
       .catch(trx.rollback)
     })
     .catch(err => res.status(400).json('unable to register'))
@@ -77,8 +83,12 @@ app.post('/register', (req, res) => {
 
 app.get('/profile/:id', (req, res) => {
   const { id } = req.params;
+  //where({id:id}) it is equal to .where({id})
+  //You search for this in knox documentation
   db.select('*').from('users').where({id})
     .then(user => {
+      //console.log(user);
+      //If length 
       if (user.length) {
         res.json(user[0])
       } else {
@@ -90,12 +100,15 @@ app.get('/profile/:id', (req, res) => {
 
 app.put('/image', (req, res) => {
   const { id } = req.body;
+  //'users' is the table name
   db('users').where('id', '=', id)
+  //increment(columns, amount)
   .increment('entries', 1)
   .returning('entries')
   .then(entries => {
     res.json(entries[0]);
   })
+  //Incase something fail
   .catch(err => res.status(400).json('unable to get entries'))
 })
 
