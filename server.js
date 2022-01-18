e020e0aef3663257a2d392378cb07d68aa08c18a
Andrei Neagoie <aneagoie@Andreis-MacBook-Pro-2.local>
2022-01-18 09:22:23 -0500

    update knexjs

--- server.js
+++ server.js
@@ -58,9 +58,9 @@ app.post('/register', (req, res) => {
         return trx('users')
           .returning('*')
           .insert({
-            // If you are using knex.js version 1.0.0 or higher it now returns an array of objects. Therefore, the code goes from:
+            // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
             // loginEmail[0] --> this used to return the email
-            // to
+            // TO
             // loginEmail[0].email --> this now returns the email
             email: loginEmail[0].email,
             name: name,
@@ -98,7 +98,7 @@ app.put('/image', (req, res) => {
     // If you are using knex.js version 1.0.0 or higher this now returns an array of objects. Therefore, the code goes from:
     // entries[0] --> this used to return the entries
     // TO
-    // entries[0].entries --> this now returns the email
+    // entries[0].entries --> this now returns the entries
     res.json(entries[0].entries);
   })
   .catch(err => res.status(400).json('unable to get entries'))
